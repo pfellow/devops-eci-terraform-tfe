7901b0010a604e7c6df169f10cbb4fadfc991269
pfellow <pfellow@gmail.com>
2024-09-30 21:52:32 +1300

    connect to github

--- locals.tf
+++ locals.tf
@@ -7,8 +7,9 @@ locals {
   workspace = {
     "devops-eci-tfe" = {
         description = "Example description of workspace"
-        execution_mode = "local"
+        execution_mode = "remote"
         project_id = module.project["devops-eci-project"].id 
+        vcs_repo_identifier = "pfellow/devops-eci-terraform-tfe" 
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "organization_name" {
     default = "test-devops-pfellow"
     type = string
 }
+variable  "github_app_installation_id" {
+    default = 55435127
+    type = number
+}
+
+variable "github_organization_name" {
+    default = "pfellow"
+    type = string
+}
