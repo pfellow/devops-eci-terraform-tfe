7b8efc3ae03194a1ba3ff73ae250e8f2950d4389
pfellow <pfellow@gmail.com>
2024-10-01 22:30:58 +1300

    setup github workspace

--- locals.tf
+++ locals.tf
@@ -9,7 +9,14 @@ locals {
         description = "Example description of workspace"
         execution_mode = "remote"
         project_id = module.project["devops-eci-project"].id 
-        vcs_repo_identifier = "pfellow/devops-eci-terraform-tfe" 
+        vcs_repo_identifier = "${var.github_organization_name}/devops-eci-terraform-tfe" 
+    }
+    "devops-eci-github" = {
+        description = "Github workspace"
+        execution_mode = "local"
+        project_id = module.project["devops-eci-project"].id 
+        vcs_repo_identifier = "${var.github_organization_name}/devops-eci-terraform-github" 
     }
   }
+  
 }
